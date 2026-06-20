64fccdb3290daed5472e96cdfbb8ec8a7902ee77
Manubolgia-Linux <manuel.obelleiro@bsc.es>
2026-06-20 13:55:00 +0200

    bug fixes

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Splendor Duel service worker: offline app shell + asset caching.
-const CACHE = 'splendor-duel-v6';
+const CACHE = 'splendor-duel-v7';
 const SHELL = ['./', './index.html', './engine.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
